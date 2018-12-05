a4c14ea1c5edf4d85ea43a94d0bcaa9c58ae864a
Mike Tinning <mike@swyftech.com>
2018-12-05 15:48:21 +0000

    Updating icon location

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyqtinstaller',
-    version='0.2.2',
+    version='0.2.3',
     packages=['pyqtinstaller'],
     install_requires=['jinja2', 'pyqtdeploy==1.3.2'],
     extras_require={
